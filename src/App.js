23b1cca15019dabbb567b5982880d8b094189cac
Hoi Won KIM <gw31513@gmail.com>
2023-05-09 12:38:39 +1200

    6.4Cleanup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  console.log("i run all the time");
-  const onChange = (event) => setKeyword(event.target.value);
+function Hello({ keyword, counter }) {
   useEffect(() => {
-    console.log("CALL THE API....");
-    console.log("I run only once.");
+    console.log("hi :)");
+    return () => console.log("bye :(");
   }, []);
+
   useEffect(() => {
     console.log("I run when 'keyword' changes.");
   }, [keyword]);
+
   useEffect(() => {
     console.log("I run when 'counter' changes.");
   }, [counter]);
+
   useEffect(() => {
     console.log("I run when keyword & counter change");
   }, [keyword, counter]);
+
+  return <h1>Hello</h1>;
+}
+
+function App() {
+  const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const [showing, setShowing] = useState(false);
+
+  const onClick = () => setCounter((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+
+  useEffect(() => {
+    console.log("I run only once.");
+    return () => {
+      console.log("bye :(");
+    };
+  }, []);
+
   return (
     <div>
       <input
@@ -29,7 +46,12 @@ function App() {
       />
       <h1>{counter}</h1>
       <button onClick={onClick}>click me</button>
+      <button onClick={() => setShowing((prev) => !prev)}>
+        {showing ? "Hide" : "Show"}
+      </button>
+      {showing ? <Hello keyword={keyword} counter={counter} /> : null}
     </div>
   );
 }
+
 export default App;
