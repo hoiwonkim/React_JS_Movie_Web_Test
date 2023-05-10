80dd92adfc7637d6b1d5d6f570b6c5e4c251639c
Hoi Won KIM <gw31513@gmail.com>
2023-05-10 13:22:01 +1200

    7.2 Coin Tracker

--- src/App.js
+++ src/App.js
@@ -1,39 +1,34 @@
-import React, { useState, useEffect } from "react";
-
-function Hello() {
-  useEffect(function () {
-    console.log("hi :)");
-    return function () {
-      console.log("bye :(");
-    };
-  }, []);
-  useEffect(() => {
-    console.log("hi :)");
-    return () => console.log("bye :(");
-  }, []);
-  return <h1>Hello</h1>;
-}
+import { useState, useEffect } from "react";
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
+  const [toDo, setToDo] = useState(""); // 할 일을 담을 state
+  const [toDos, setToDos] = useState([]); // 할 일 목록을 담을 state
+  const onChange = (event) => setToDo(event.target.value); // input 값이 변경될 때마다 실행되는 함수
   const onSubmit = (event) => {
-    event.preventDefault();
+    // form이 제출될 때 실행되는 함수
+    event.preventDefault(); // 기본 동작 방지
     if (toDo === "") {
-      return;
+      // 할 일이 입력되지 않았을 경우
+      return; // 함수 종료
     }
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo("");
+    setToDos((currentArray) => [toDo, ...currentArray]); // 할 일 목록에 새로운 할 일 추가
+    setToDo(""); // input 값 초기화
   };
-  console.log(toDos);
-  console.log(toDos.map((item, index) => <li key={index}>{item}</li>));
+  console.log(toDos); // 할 일 목록 출력
+  console.log(toDos.map((item, index) => <li key={index}>{item}</li>)); // 할 일 목록을 li 태그로 변환하여 출력
+  const [loading, setLoading] = useState(true); // API 요청 중인지 여부를 담을 state
+  const [coins, setCoins] = useState([]); // API 요청 결과를 담을 state
+  useEffect(() => {
+    // 컴포넌트가 마운트될 때 한 번만 실행되는 함수
+    fetch("https://api.coinpaprika.com/v1/tickers") // API 요청
+      .then((response) => response.json()) // 응답을 json 형태로 변환
+      .then((json) => {
+        setCoins(json); // API 요청 결과를 state에 저장
+        setLoading(false); // API 요청 완료
+      });
+  }, []);
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
       <h1>My To Dos ({toDos.length})</h1>
       <form onSubmit={onSubmit}>
         <input
@@ -50,8 +45,22 @@ function App() {
           <li key={index}>{item}</li>
         ))}
       </ul>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select>
+          {coins.map((coin) => (
+            <option>
+              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+            </option>
+          ))}
+        </select>
+      )}
     </div>
   );
 }
-
+// App.js 파일에서 App 컴포넌트를 export합니다.
+// 이 컴포넌트는 애플리케이션의 루트 컴포넌트입니다.
+// 다른 컴포넌트들을 이 컴포넌트 안에서 렌더링합니다.
 export default App;
