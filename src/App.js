93d80a47997e18274adbc60dc90b4fb60cbdbc91
Hoi Won KIM <gw31513@gmail.com>
2023-05-23 12:16:56 +1200

    7.6Parameters

--- src/App.js
+++ src/App.js
@@ -6,8 +6,11 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/hello" element={<h1>Hello</h1>} />
-        <Route path="/movie" element={<Detail />} />
+        <Route path="/hello" element={<h1>Hello</h1>}>
+          <Route path="about-us" element={<h1>About Us</h1>} />
+          <Route path="movie" element={<h1>Movies</h1>} />
+          <Route path="movie/:id" element={<Detail />} />
+        </Route>
         <Route path="/" element={<Home />} />
       </Routes>
     </Router>
